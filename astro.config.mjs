e2700e11f65c6f7971bd1eedd41e11ce3d19dae1
Edward Tan <guofoo@gmail.com>
2025-10-06 20:45:55 -0700

    Fix base path condition in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
   site: 'https://gosimfoundation.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/gosim-vision-forum-2025' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   i18n: {
     defaultLocale: "zh",
     locales: ["zh", "en"],
